1d9ffd32f0d33f9af2bad5517194b228d3d4eab0
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2023-10-28 15:55:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function setup() {
   talkingGirl.addAnimation('talking', girl_talking);
   talkingGirl.animation.frameDelay = 15; // Set the frame delay
   talkingGirl.scale = 0.4
+  talkingGirl.hide()
   createSprites();
   backgroundMusic.setVolume(0.5)
   backgroundMusic.play()
